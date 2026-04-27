7cfb47801015a5f9e40f9409c56730862cba9b47
gabrielcggs <gabrielcggsilva@gmail.com>
2026-04-27 12:48:11 -0300

    ORGANIZEI E FIZ OS EX 1 E 2 DA AULA 4

---
+++ AULA04/EX1.py
@@ -0,0 +1,6 @@
+while True:
+    print("Olá, mundo")
+    jogar = input("Deseja exibir a mensagem novamente? (sim/não) ")
+    if jogar.lower() == "não":
+        print("Fim")
+        break
---
+++ AULA04/EX3.py
@@ -0,0 +1,6 @@
+n = int(input("Digite um numero inteiro positivo n: "))
+cont = 1
+while cont <= 25:
+    tabuada = n * cont
+    print(tabuada)
+    cont += 1
